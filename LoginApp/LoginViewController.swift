f0ea05c72acd9a6ac4dbb3df341fc4fe20ac9d84
Egor Ledkov <egor@ledkov.org>
2022-07-01 00:54:11 +0300

    Create UI for Login View

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Egor Ledkov on 01.07.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        userNameTF.resignFirstResponder()
+        passwordTF.resignFirstResponder()
+    }
+
+    @IBAction func loginPressed() {
+
+    }
+    
+    @IBAction func hintPressed() {
+        
+    }
+    
+}
+
