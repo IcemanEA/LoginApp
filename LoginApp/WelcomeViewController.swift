b29eba06b4781a37245896cd8258c11e77057e52
Egor Ledkov <egor@ledkov.org>
2022-07-01 20:20:43 +0300

    Add gradient

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 class WelcomeViewController: UIViewController {
         
     @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var gradientView: UIView!
     
     var username: String!
+    
+    private let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         welcomeLabel.text = "Welcome, \(username ?? "")!"
+        
+        gradientView.layer.addSublayer(gradientLayer)
+        gradientLayer.colors = [UIColor.systemPink.cgColor, UIColor.systemBlue.cgColor]
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = view.frame
     }
 }
